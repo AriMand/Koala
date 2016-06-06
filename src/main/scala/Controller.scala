574affb28a8a7aac0f2312cc4717573f29c8edd7
arimand <aritramandalkgp@gmail.com>
2016-06-06 12:30:50 +0530

    added consumer info Extraction

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -4,6 +4,8 @@ import KafkaUtils.KafkaMetrics
 import org.I0Itec.zkclient._
 import KafkaUtils.TopicInfo
 import KafkaUtils.BrokerInfo
+import KafkaUtils.ConsumerInfo
+import KafkaUtils.ConsumerGroupInfo
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
@@ -13,7 +15,10 @@ case class Metrics(topics: Seq[TopicInfo], brokers:Seq[BrokerInfo])
 object KafkaJsonProtocol extends DefaultJsonProtocol {
   implicit val brokerFormat = jsonFormat(BrokerInfo, "id", "host", "port")
   implicit val topicFormat = jsonFormat(TopicInfo, "name", "partition","leader","replicas","inSyncReplicas")
+  implicit val consumerFormat = jsonFormat(ConsumerInfo, "consumerName", "threadCount","topicName")
+  implicit val consumerGroupFormat = jsonFormat(ConsumerGroupInfo, "consumerGroupName", "consumerCount","topicCount","consumers")
 }
+
 import KafkaJsonProtocol._
 
 object Main extends App with SimpleRoutingApp {
@@ -28,10 +33,11 @@ object Main extends App with SimpleRoutingApp {
           //var km=new KafkaMetrics(new ZkClient("edhl3n5:2181"), new ZkConnection("edhl3n5:2181"))
           val topics=km.listTopics
           val brokers=km.listBrokers
+          val consumerGroups=km.listConsumerGroups
           val met=Metrics(topics,brokers)
-          val metrics=JsObject(Map("topics" -> JsArray(topics.map(_.toJson).toList),"brokers" -> JsArray(brokers.map(_.toJson).toList)))
-          //metrics.toString()
-          <h1>Hello World</h1>
+          val metrics=JsObject(Map("topics" -> JsArray(topics.map(_.toJson).toList),"brokers" -> JsArray(brokers.map(_.toJson).toList),"consumerGroups"->JsArray(consumerGroups.map(_.toJson).toList)))
+          metrics.toString()
+          //<h1>Hello World</h1>
         }
       }
     }
